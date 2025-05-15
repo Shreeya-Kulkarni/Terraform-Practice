4051dee63ef1d308cb88e877bc82113381d14009
Shreeya <shreeyakulkarni03@gmail.com>
2025-05-15 19:57:10 +0530

    terraform file

--- terraform.tf
+++ terraform.tf
@@ -1,11 +1,14 @@
 provider "aws" {
-  region = "us-west-2"
+
+   region = "us-east-1"  
 }
 
+
+
 resource "aws_instance" "web" {
-  ami           = "ami-08962a4068733a2b6"  # valid for us-west-2
+  ami           = "ami-0f88e80871fd81e91"
   instance_type = "t2.micro"
-  key_name      = "new-key"
+  key_name = "new-key"
 
   tags = {
     Name = "HelloWorld"

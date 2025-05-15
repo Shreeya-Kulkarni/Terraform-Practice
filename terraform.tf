439f78d1b9133cac72d9b2874d16fc4425e5ba8e
Shreeya <shreeyakulkarni03@gmail.com>
2025-05-15 20:48:23 +0530

    terraform file

--- terraform.tf
+++ terraform.tf
@@ -7,7 +7,7 @@ provider "aws" {
 
 resource "aws_instance" "web" {
   ami           = "ami-0f88e80871fd81e91"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
   key_name = "new-key"
 
   tags = {

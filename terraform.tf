66bd7b060b48ea3fdacbb672812ff376110cbbc7
Shreeya <shreeyakulkarni03@gmail.com>
2025-05-16 20:19:20 +0530

    terraform file

--- terraform.tf
+++ terraform.tf
@@ -7,6 +7,7 @@ resource "aws_instance" "web" {
   ami           = "ami-0e35ddab05955cf57"
   instance_type = "t3.nano"
   key_name = "id-rsa"
+  vpc_security_group_ids = [ "sg-0491e5dd427a715ed" ]
 
   tags = {
     Name = "HelloWorld"

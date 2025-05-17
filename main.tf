1071d3397fa6dde3b728f0d0948a482aaa918135
Shreeya <shreeyakulkarni03@gmail.com>
2025-05-17 21:01:52 +0530

    terraform Vpc

---
+++ main.tf
@@ -0,0 +1,99 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-practice-shreeya"
+    region = "ap-south-1"
+    key = "terraform.tfstate"
+  }
+}
+
+resource "aws_vpc" "myvpc"{
+cidr_block = var.vpc_cidr
+    tags = {
+      Name = var.vpcname
+      project = var.project_name
+    }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id = aws_vpc.myvpc.id
+  cidr_block = var.pub_cidr
+   tags = {
+      Name = "${var.project_name}_public_subnet"
+      project = var.project_name
+    }
+    map_public_ip_on_launch = true 
+    availability_zone = "ap-south-1b"
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id = aws_vpc.myvpc.id
+  cidr_block = var.pvt_cidr
+   tags = {
+      Name = "private_subnet"
+      project = var.project_name
+    }
+    map_public_ip_on_launch = false
+    availability_zone = "ap-south-1a"
+}
+
+resource "aws_internet_gateway" "myigw" {
+  vpc_id = aws_vpc.myvpc.id
+  tags = {
+      Name = "igw"
+      project = var.project_name
+    } 
+}
+
+resource "aws_route_table" "mytable" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route{
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myigw.id
+  }
+
+ tags = {
+      Name = "Route_Table"
+      project = var.project_name
+    } 
+}
+
+
+data "aws_security_group" "mysg" {
+    filter {
+      name = "group_name"
+      values = [mysg]
+    }
+  
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0e35ddab05955cf57"
+  instance_type = var.instance_type
+  key_name = "id-rsa"
+  vpc_security_group_ids = data.aws_security_group.mysg.id
+
+  connection {
+    type = "ssh"
+    user = "root"
+    password = password
+    host = self.public_ip
+  }
+
+  provisioner "file"{
+    source = "index.html"
+    destination = "var/www/html/index.html"
+    
+}
+
+  provisioner "local-exec" {
+    command = "echo this task is successfull"
+  }
+
+  tags = {
+    Name =  var.project_name
+    
+  }
+ 
+}
+
--- terraform.tf
+++ terraform.tf
@@ -1,33 +1,32 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-practice-shreeya"
-    region = "ap-south-1"
-    key = "terraform.tfstate"
-  }
-}
 
-provider "aws" {
+# terraform {
+#   backend "s3" {
+#     bucket = "terraform-practice-shreeya"
+#     region = "ap-south-1"
+#     key = "terraform.tfstate"
+#   }
+# }
 
-   region = "ap-south-1"  
-}
+# provider "aws" {
 
-resource "aws_instance" "web" {
-  ami           = "ami-0e35ddab05955cf57"
-  instance_type = var.instance_type
-  key_name = "id-rsa"
-  vpc_security_group_ids = [ "sg-0491e5dd427a715ed" ]
+#    region = "ap-south-1"  
+# }
 
-  tags = {
-    Name = "HelloWorld"
-  }
+# resource "aws_instance" "web" {
+#   ami           = "ami-0e35ddab05955cf57"
+#   instance_type = var.instance_type
+#   key_name = "id-rsa"
+#   vpc_security_group_ids = [ "sg-0491e5dd427a715ed" ]
+
+#   tags = {
+#     Name = "HelloWorld"
+#   }
  
-}
-
-variable "instance_type" {
-  default = "t2.micro"
-  description = "this is basc instance"
-}
-
-output "public_ip" {
-   value = aws_instance.web.public_ip
-}
+# }
+# variable "instance_type" {
+#   default = "t2.micro"
+#   description = "this is basc instance"
+# }
+# output "public_ip" {
+#    value = aws_instance.web.public_ip
+# }
